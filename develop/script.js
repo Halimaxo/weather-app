1505ffb614233e33248738017be50f6bccabaf45
halimaxo <halimakshuchi@gmail.com>
2023-03-12 19:59:15 -0500

    html page

---
+++ develop/script.js
@@ -0,0 +1,3 @@
+//ed9e74b645329e040d2d7fc1db70ac25
+//create an event listener upon clicking the search button. Whatever the value is in the input field will be the city name
+//create function for getting current data and weather info
