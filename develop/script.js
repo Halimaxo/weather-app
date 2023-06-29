0faa8637a2bb860388e94c0c35e787052caa831f
halimaxo <halimakshuchi@gmail.com>
2023-06-29 17:59:34 -0500

    http change

--- develop/script.js
+++ develop/script.js
@@ -22,7 +22,7 @@ function getWeatherData() {
   var searchLocation = inputField.value;
 
   var latAndLon =
-    "https://api.openweathermap.org/geo/1.0/direct?q=" +
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
     searchLocation +
     "&limit=1&appid=f1483ebfebd864431fd44f2588c040e8";
 
@@ -45,7 +45,7 @@ function getWeatherData() {
       // console.log(lat, lon, latAndLonData);
 
       var weatherApi =
-        "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+        "http://api.openweathermap.org/data/2.5/forecast?lat=" +
         lat +
         "&lon=" +
         lon +
